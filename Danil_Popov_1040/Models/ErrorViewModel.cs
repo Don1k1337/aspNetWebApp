a9de26a979a765b70cd43cdcfd637da44b0fdd9f
Danil <46310990+Don1k1337@users.noreply.github.com>
2021-03-02 16:04:21 +0600

    Add project files.

---
+++ Danil_Popov_1040/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Danil_Popov_1040.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
